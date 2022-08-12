9f88f84e70f7a0d17259db433ab62df59da64b68
semaaJ <mamesjiles@gmail.com>
2022-08-12 14:35:45 +0100

    blackjack base game + components

--- src/App.js
+++ src/App.js
@@ -1,55 +1,67 @@
 import React, { useState } from 'react';
 import Card from './components/Card/Card';
 import Gamebar from './components/Gamebar/Gamebar';
-import { startGame, initialiseGame, playerBet, playerHit, playerStand, nextHand  } from './utils/BlackJack';
-import Button from './components/Button/Button';
-
+import { startGame, initialiseGame, playerBet, playerHit, playerStand, nextHand, playerDoubleDown  } from './utils/BlackJack';
 import './App.css';
 
+export const renderHouseCards = (cards, showSecond) => {
+  return cards[0] ? (
+    <>
+      { !showSecond && <Card rank={cards[0].rank} symbol={cards[0].symbol} colour={cards[0].colour} /> }
+      { showSecond && cards.map(c => <Card rank={c.rank} symbol={c.symbol} clour={c.colour} />)}
+    </>
+  ) : (
+    Array.from(Array(3).keys()).map(i => <div className="border-2 border-gray-800 px-3 shadow-xl ring-1 ring-gray-900/5 w-40 h-60 m-2" /> )
+  ) 
+}
+
+export const renderPlayerCards = (cards) => {
+  return (
+    <>
+      { cards.map(card => <Card rank={card.rank} symbol={card.symbol} colour={card.colour} />) }
+      { Array.from(Array(5 - cards.length).keys()).map(i =>  
+        <div className="border-2 border-gray-800 px-3 shadow-xl ring-1 ring-gray-900/5 w-40 h-60 m-2" /> )
+      }
+    </>
+  )
+}
+
+export const bet = (state, setState, amount) => setState(playerBet(state, amount));
+export const hit = (state, setState) => setState(playerHit(state));
+export const doubleDown = (state, setState) => setState(playerDoubleDown(state));
+export const stand = (state, setState) => {
+  setState({ ...state, showSecond: true });
+  setState(playerStand(state));
+}
+export const next = (state, setState) => {
+  if (!state.inPlay) setState(startGame(state))
+  else setState(nextHand(state, state.showSecond));  
+}
 
 function App() {
   const [state, setState] = useState(initialiseGame());
-
-  const resetState = () => setState(initialiseGame())
-
-  const start = () => setState(startGame(state));
-  const bet = (amount) => setState(playerBet(state, 100));
-  const hit = () => setState(playerHit(state));
-  const stand = () => setState(playerStand(state));
-  const next = () => setState(nextHand(state));  
-
-  const firstCard = state.house.cards[0];
-
+  
   return (
     <div className="w-screen h-screen flex flex-col items-center bg-slate-700">
         <div className="flex flex-col justify-evenly h-full max-w-7xl">
           <div className="flex justify-center">
-            { firstCard ? (
-              <Card rank={firstCard.rank} symbol={firstCard.symbol} colour={firstCard.colour} />
-            ) :(
-               Array.from(Array(3 - state.house.cards.length).keys()).map(i =>         
-                <div className="cursor-pointer border-2 border-gray-800 px-3 shadow-xl ring-1 ring-gray-900/5 w-40 h-60 m-4" /> )
-              )
-            }
+            { renderHouseCards(state.house.cards, state.showSecond) }
           </div>
 
           <div className="flex items-center justify-center">
             <Gamebar 
-              start={start} 
               bet={bet} 
               hit={hit} 
+              doubleDown={doubleDown}
               stand={stand} 
               next={next}
               state={state} 
+              setState={setState}
             />
           </div>
-          <Button onClick={resetState} />
 
           <div className="flex justify-center">
-            { state.player.cards.map(card => <Card rank={card.rank} symbol={card.symbol} colour={card.colour} />) }
-            { Array.from(Array(5 - state.player.cards.length).keys()).map(i =>  
-              <div className="cursor-pointer border-2 border-gray-800 px-3 shadow-xl ring-1 ring-gray-900/5 w-40 h-60 m-4" /> )
-            }
+            { renderPlayerCards(state.player.cards) }
           </div>
         </div> 
     </div>
--- src/components/Gamebar/Gamebar.js
+++ src/components/Gamebar/Gamebar.js
@@ -1,48 +1,65 @@
 import Button from '../Button/Button';
 
+export const CHIPS = [
+    { "value": 5, imageUrl: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1151732/chip-5.png", alt: "Chip 5 value" },
+    { "value": 10, imageUrl: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1151732/chip-7.png", alt: "Chip 10 value" },
+    { "value": 15, imageUrl: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1151732/chip-4.png", alt: "Chip 15 value" },
+    { "value": 50, imageUrl: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1151732/chip-3.png", alt: "Chip 50 value" }
+]
+
+export const getGameStateText = (state) => {
+    if (state.player.isBlackJack && !state.tie) {
+        return "BLACKJACK! YOU WIN"
+    } else if (state.tie) {
+        return "TIE"
+    } if (state.playerBust || state.houseBust) {
+        return state.playerBust ? "YOU WENT BUST, DEAL CARDS" : "THE HOUSE WENT BUST, DEAL CARDS";
+    } else if (state.playerWins || state.houseWins) {
+        return state.playerWins ? "YOU WON, DEAL CARDS" : "THE HOUSE WON, DEAL CARDS";
+    } else if (!state.inPlay) {
+        return "DEAL CARDS"
+    } else if (state.initialBet && state.inPlay) {
+        return "MAKE A BET"
+    } else {
+        return "HIT, DOUBLE DOWN OR STAND"
+    }
+}
+
 const Gamebar = (props) => {
-    const { start, next, bet, hit, stand, state } = props;
-
-    // TODO: tidy up
-    if (state.player.isBust) {
-        return (
-            <>
-                <h1 className="text-white text-2xl px-8">YOU WENT BUST!</h1>
-                <Button text="Deal" onClick={next} />
-            </>
-        )
-    }
-
-    if (state.house.isBust) {
-        return (
-            <>
-                <h1 className="text-white text-2xl px-8">HOUSE WENT BUST!</h1>
-                <Button text="Deal" onClick={next} />
-            </>
-        )
-    }
-
-    if (!state.inPlay) { 
-        return (
-            <Button text="Deal" onClick={start} />
-        )   
-    }
-
-    if (state.initialBet) {
-        return (
-            <Button text="Bet" onClick={bet} />
-        )
-    }
-
+    const { next, bet, hit, stand, doubleDown, state, setState } = props;
+    const disabled = state.initialBet || state.houseWins || state.playerWins;
+    
     return (
-        <>
-            <Button text="Bet" onClick={bet} />
-            <Button text="Hit" onClick={hit} />
-            <Button text="Stand" onClick={stand} />
-            <h1 className="text-white text-2xl px-8">Bank: ${ state.player.bank }</h1>
-            <h1 className="text-white text-2xl px-8">Pot: ${ state.pot }</h1>
-            <h1 className="text-white text-2xl px-8">Player/House Score: { state.player.score }/{ state.house.score }</h1>
-        </>
+        <div className="flex flex-col">
+            <div className="text-center w-full mb-2">
+                <h1 className="text-white text-2xl">{ getGameStateText(state) }</h1>
+            </div>
+
+            <div className="flex justify-between">
+                <Button text="DEAL" disabled={state.inPlay} onClick={() => next(state, setState)}  />
+                <Button text="HIT" disabled={disabled} onClick={() => hit(state, setState)}  />
+                <Button text="DOUBLE DOWN"  disabled={disabled} onClick={() => doubleDown(state, setState)} />
+                <Button text="STAND"  disabled={disabled} onClick={() => stand(state, setState)} />
+            </div>
+
+            <div className="flex justify-evenly bg-gray-200 items-center">
+                <div className="flex text-center flex-col">
+                    <h1 className="text-gray-700 text-3xl font-semibold">BET</h1>
+                    <h1 className="text-gray-700 text-xl font-semibold">${ state.pot }</h1>
+                </div>
+
+                <div className="flex justify-center py-4 mt-2 px-2">
+                    { CHIPS.map((chip, i) => (
+                        <img key={i} className="ml-4 mr-4 w-16 h-16 cursor-pointer" src={chip.imageUrl} alt={chip.alt} onClick={() => bet(state, setState, chip.value)} />
+                    )) }
+                </div>
+
+                <div className="flex text-center flex-col">
+                    <h1 className="text-gray-700 text-3xl font-semibold">CHIPS</h1>
+                    <h1 className="text-gray-700 text-xl font-semibold">${ state.player.bank }</h1>
+                </div>
+            </div>
+        </div>
     )
 }
 
